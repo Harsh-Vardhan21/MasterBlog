1e5fb89cfe727d10f06bde3aebea860b0bb8c67b
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-02-11 18:32:18 +0530

    update

---
+++ api/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
+
+dotenv.config();
+const app = express();
+
+
+
+mongoose.connect(
+    process.env.MONGO
+).then(()=>{
+    console.log("DB is Connected !");
+}).catch((err)=>{
+    console.log("Error :-" , err);
+});
+
+
+app.use(express.json())
+app.listen(3000 , () => {
+    console.log('Server is running on PORT:3000');
+})
+
+app.use('/api/user' , userRoutes)
+app.use('/api/auth' , authRoute)
+
+app.use((error , req , res , next)=>{
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'Iternal Server Error'
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
+})
+
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Header from './components/Header'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <h1 className='font-extrabold'>Harsh vardhan</h1>
-    </>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
