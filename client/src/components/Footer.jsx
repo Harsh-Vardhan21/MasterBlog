b970125753dd2360f73a6cdda2a29396b187d087
Harsh Vardhan <harsh.1vardhan123@gmail.com>
2024-05-28 23:38:31 +0530

    fixing

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
-import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+import {  BsInstagram, BsTwitter, BsGithub} from 'react-icons/bs';
 export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
@@ -21,18 +21,11 @@ export default function FooterCom() {
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.100jsprojects.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  100 JS Projects
-                </Footer.Link>
-                <Footer.Link
                   href='/about'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand's Blog
+                  MasterBlog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -40,7 +33,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://github.com/Harsh-Vardhan21'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
@@ -62,15 +55,13 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Sahand's blog"
+            by="MasterBlog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-            <Footer.Icon href='#' icon={BsFacebook}/>
-            <Footer.Icon href='#' icon={BsInstagram}/>
-            <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/>
-            <Footer.Icon href='#' icon={BsDribbble}/>
+            <Footer.Icon href='https://www.instagram.com/harsh_._vardhan_' icon={BsInstagram}/>
+            <Footer.Icon href='https://x.com/HarshVar21' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com/Harsh-Vardhan21' icon={BsGithub}/>
 
           </div>
         </div>
